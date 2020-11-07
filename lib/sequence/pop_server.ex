bafd4b4153119e0de627c9328c18d6567b28d988
Matt Galvin <matthewgalvin908@gmail.com>
2020-11-07 14:12:11 +0100

    Wrap Genserver commands in PopServer module; add termination/2 override

--- lib/sequence/pop_server.ex
+++ lib/sequence/pop_server.ex
@@ -1,8 +1,23 @@
 defmodule Sequence.PopServer do
   use GenServer
 
+  #####
+  # External API
+
+  def start_link(initial_list) do
+    GenServer.start_link(__MODULE__, initial_list, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+
+  def push(thing) do
+    GenServer.cast __MODULE__, {:push, thing}
+  end
+
   def init(initial_list) do
-    { :ok, initial_list }
+    {:ok, initial_list}
   end
 
   def handle_call(:pop, _from, [_h|t] = current_list) do
@@ -10,7 +25,11 @@ defmodule Sequence.PopServer do
   end
 
   def handle_cast({:push, thing}, current_list) do
-    { :noreply, [thing|current_list]}
+    {:noreply, [thing|current_list]}
+  end
+
+  def terminate(reason, state) do
+    IO.puts "Terminate because #{inspect reason}, state at exit #{inspect state}"
   end
 
 end
